443871cd868aa3dcde136c74326d87c1e2c2de2f
Andrew Smith <andrewsmith@Andrews-iMac.local>
2016-09-29 02:11:48 -0400

    Active panels and refactored into a PanelController

--- angularjs_app1/js/app.js
+++ angularjs_app1/js/app.js
@@ -15,6 +15,16 @@
       name: 'Dodecahedron',
       price: 2.95,
       description: 'Some gems have hidden qualities beyond their luster, beyond their shine... Dodeca is one of those gems.',
+      images: [
+        {
+          full: 'images/dodecahedron.gif',
+          thumb: ''
+        },
+        {
+          full: 'images/dodecahedron2.gif',
+          thumb: ''
+        }
+      ],
       canPurchase: true,
       soldOut: true
     },
@@ -22,7 +32,29 @@
       name: "Pentagonal Gem",
       price: 5.95,
       description: "Origin of the Pentagonal Gem is unknown, hence its low value.  It has a very high shine and 12 sides, however.",
+      images: [
+        {
+          full: 'images/pentagon.gif',
+          thumb: ''
+        },
+        {
+          full: 'images/pentagon2.png',
+          thumb: ''
+        }
+      ],
       canPurchase: false
     }
   ];
+
+  app.controller("PanelController", function() {
+    // Initial value of tab is 1.
+    this.tab = 1;
+    // Update the tab's value and feed it to ng-click in the list.
+    this.selectTab = function(setTab) {
+      this.tab = setTab;
+    };
+    this.isSelected = function(checkTab) {
+      return this.tab === checkTab;
+    };
+  });
 })();
