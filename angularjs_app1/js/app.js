121af77ebf0167429fbf9ad3ac7c02e2cd3c22fb
Andrew Smith <andrew.d.e.smith@gmail.com>
2016-09-30 12:45:00 -0400

    Add active tabs to each store item  and refactor code into PanelController

--- angularjs_app1/js/app.js
+++ angularjs_app1/js/app.js
@@ -10,11 +10,32 @@
     this.products = gems;
   });
 
+  app.controller("PanelController", function() {
+    this.tab = 1;
+    
+    this.selectTab = function(setTab) {
+      this.tab = setTab;
+    };
+    this.isSelected = function(checkTab) {
+      return this.tab === checkTab;
+    };
+  });
+
   var gems = [
     {
-      name: 'Dodecahedron',
+      name: 'Dodecahedron Gem',
       price: 2.95,
       description: 'Some gems have hidden qualities beyond their luster, beyond their shine... Dodeca is one of those gems.',
+      images: [
+        {
+          full: 'images/gem-04.gif',
+          thumb: ''
+        },
+        {
+          full: '',
+          thumb: ''
+        }
+      ],
       canPurchase: true,
       soldOut: true
     },
@@ -22,6 +43,16 @@
       name: "Pentagonal Gem",
       price: 5.95,
       description: "Origin of the Pentagonal Gem is unknown, hence its low value.  It has a very high shine and 12 sides, however.",
+      images: [
+        {
+          full: 'images/gem-03.gif',
+          thumb: ''
+        },
+        {
+          full: '',
+          thumb: ''
+        }
+      ],
       canPurchase: false
     }
   ];
