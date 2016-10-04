98ad1549c45baefa2d700bad19d0ea98241a3901
Andrew Smith <andrew.d.e.smith@gmail.com>
2016-10-04 15:51:27 -0400

    Refactor code using custom directives and Controller aliases

--- angularjs_app1/js/app.js
+++ angularjs_app1/js/app.js
@@ -30,6 +30,39 @@
     };
   });
 
+  // Dash in HTML (product-title) translates to camelCase (productTitle) in JavaScript.
+  app.directive('productTitle', function() {
+    // Returns a directive definition object: a configuration object defining how your directive will work.
+    return {
+      // Specifiy type of directive (here, 'E' for element).
+      restrict: 'E',
+      // What template page will this directive load into?
+      templateUrl: 'product-title.html'
+    };
+  });
+
+  app.directive("productPanels", function() {
+    return {
+      restrict: 'E',
+      templateUrl: 'product-panels.html',
+      controller: function() {
+        app.controller("PanelController", function() {
+          // Initial value of tab is 1.
+          this.tab = 1;
+          // Update the tab's value and feed it to ng-click in the list.
+          this.selectTab = function(setTab) {
+            this.tab = setTab;
+          };
+          this.isSelected = function(checkTab) {
+            return this.tab === checkTab;
+          };
+        });
+      },
+      // Enables use of panels as our Controller alias.
+      controllerAs: 'panels'
+    };
+  });
+
   var gems = [
     {
       name: 'Dodecahedron Gem',
@@ -78,16 +111,4 @@
       canPurchase: false
     }
   ];
-
-  app.controller("PanelController", function() {
-    // Initial value of tab is 1.
-    this.tab = 1;
-    // Update the tab's value and feed it to ng-click in the list.
-    this.selectTab = function(setTab) {
-      this.tab = setTab;
-    };
-    this.isSelected = function(checkTab) {
-      return this.tab === checkTab;
-    };
-  });
 })();
